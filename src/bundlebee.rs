56fe4d0ac5317f56859234a16cd3cd2850cc0ca4
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2022-06-08 13:49:27 -0400

    added the prefix file path making the CI test fail

--- src/bundlebee.rs
+++ src/bundlebee.rs
@@ -1,23 +1,38 @@
-use serde_json::json;
+use std;
+use serde_json::{self, json};
+
 pub struct BundleBee {}
 
 impl BundleBee {
     pub fn new() -> Self {
-        Self {}
+        Self {  }
     }
 
     pub fn create_config_file(&self, config_file_path: &str) {
-        let config_file_content = json!({
-            "imports": [],
-            "exports": [],
+        let path = std::path::Path::new(config_file_path);
+
+        let prefix = match path.parent() {
+            Some(p) => p.to_str().unwrap().to_string(),
+            None => "./".to_string(),
+        };
+
+        match std::fs::create_dir_all(prefix) {
+            Ok(_) => {},
+            Err(e) => {
+                println!("{}", e);
+            },
+        }
+
+        let config = json!({
+            "imports" : [],
+            "exports" : []
         });
-        let config_file_content_str = match serde_json::to_string(&config_file_content) {
-            Ok(content) => content,
-            Err(e) => panic!("{}", e),
-        };
-        match std::fs::write(config_file_path, config_file_content_str) {
-            Ok(_) => (),
-            Err(e) => panic!("{}", e),
-        };
+        
+        match std::fs::write(config_file_path, config.to_string()){
+            Ok(_) => {},
+            Err(e) => {
+                println!("{}", e);
+            },
+        }
     }
 }
